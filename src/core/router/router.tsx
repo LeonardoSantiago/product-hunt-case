f0e0b0aea7d88eb62f972fca3faf3610b5e8c493
Leonardo <leonardomss_12@hotmail.com>
2025-05-07 22:50:56 -0300

    fix - adjust routes

--- src/core/router/router.tsx
+++ src/core/router/router.tsx
@@ -1,15 +1,26 @@
 import { Posts, PostsDetails } from "@pages";
-import type { RouteObject } from "react-router";
+import { Navigate, type RouteObject } from "react-router";
 
-export const router: RouteObject[] = [{
-    path: "/posts",
-    element: <Posts />
-},
-{
-    path: "/posts/newest",
-    element: <Posts />
-},
-{
-    path: "post/:id",
-    element: <PostsDetails />
-}];
+export const router: RouteObject[] = [
+    {
+        path: "/posts",
+        element: <Posts />
+    },
+    {
+        path: "/posts/newest",
+        element: <Posts />
+    },
+    {
+        path: "post/:id",
+        element: <PostsDetails />
+    },
+    {
+        path: '/',
+        element: <Navigate to="/posts" replace />,
+    },
+    {
+        path: '*',
+        element: <Navigate to="/posts" replace />,
+    },
+
+];
